8e24c9500ff224b7adffc1f0d657a3dd130ecdf6
aviel <avielas@gmail.com>
2021-11-15 22:51:37 +0200

    remove old design file

--- src/Consts.java
+++ src/Consts.java
@@ -1,6 +1,5 @@
 public class Consts {
     public static final double PIXEL_INTENSITY_VALUE = 0.5;
-//    public static final int HOLE_VALUE = -1;
     public static final float HOLE_VALUE = -1f;
 
 }
