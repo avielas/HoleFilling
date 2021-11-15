04e18fb2bb1d83fd736110b10c69861e70b4fa16
aviel <avielas@gmail.com>
2021-11-15 10:21:17 +0200

    all project

---
+++ src/Consts.java
@@ -0,0 +1,6 @@
+public class Consts {
+    public static final double PIXEL_INTENSITY_VALUE = 0.5;
+    public static final int HOLE_VALUE = -1;
+
+
+}
---
+++ src/Pixel.java
@@ -0,0 +1,32 @@
+/**
+ * A Point obj which holds a float val that's in range [0,1]
+ */
+
+public class Pixel {
+    private int x;
+    private int y;
+    private float val;
+
+
+    public Pixel(int y, int x, float value) {
+        this.x = x;
+        this.y = y;
+        this.val = value;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setVal(float val) {
+        this.val = val;
+    }
+
+    public float getVal() {
+        return val;
+    }
+}
