66a7d4a58415951fc61e1b212eb4430c60e54c35
aviel <avielas@gmail.com>
2021-11-18 12:15:46 +0200

    trying to cut B to sectors for O(n) optimization at Q2

--- src/HoleFillingCalculator.java
+++ src/HoleFillingCalculator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Set;
 
 
@@ -8,31 +9,47 @@ public class HoleFillingCalculator implements IHoleFillingCalculator {
      * @param image
      */
     public static void fillHole(HoledImage image) {
+        ArrayList<Pixel> bMeanColor = calcBmeanColor(127, image.getHole().getBoundary());
         for (Pixel h : image.getHole().getPixels()) {
-            h.setVal(calcColor(h, image.getHole().getBoundary(), image.getHole().getWeightFunc()));
+            h.setVal(calcColor(h, image.getHole().getWeightFunc(), bMeanColor));
         }
     }
 
-    private static float calcBmeanColor(Set<Pixel> B) {
+    private static ArrayList<Pixel> calcBmeanColor(int sections, Set<Pixel> B) {
+        ArrayList<Pixel> bMeanColorL = new ArrayList<>();
+        int n = B.size();
         float sum = 0;
+        int count = 0, countN = 0;
+
         for (Pixel b : B) {
             sum+=b.getVal();
+            count++;
+            countN++;
+            if(count > n/sections || countN == n){
+                float avr = sum/count;
+                bMeanColorL.add(new Pixel(b.getY(), b.getX(), avr));
+                sum = 0;
+                count = 0;
+            }
         }
-        return sum/B.size();
+
+
+
+        return bMeanColorL;
     }
 
     /**
      *
      * @param h
-     * @param B
+     *
      * @param w
      * @return
      */
-    private static float calcColor(Pixel h, Set<Pixel> B, IWeightFunc w) {
+    private static float calcColor(Pixel h, IWeightFunc w, ArrayList<Pixel> bMeanColor) {
         float numerator = 0;
         float denominator = 0;
 
-        for (Pixel b : B) {
+        for (Pixel b : bMeanColor) {
             float weight = w.weight(h, b);
             numerator += weight * b.getVal();
             denominator += weight;
